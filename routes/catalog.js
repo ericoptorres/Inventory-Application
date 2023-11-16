3308e4c07f949c713528f6aa187823d1a77ad832
erico <erico_pt@hotmail.com>
2023-11-16 19:24:46 -0300

    added Category routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const part_controller = require('../controllers/partController')
 const part_instance_controller = require('../controllers/partInstanceController')
 const category_controller = require('../controllers/categoryController')
+const category = require('../models/category')
 
 /// PARTS ROUTES ///
 
@@ -29,4 +30,29 @@ router.get('/part/:id', part_controller.part_detail)
 
 //GET request for list of all Parts
 router.get('/parts', part_controller.part_list)
+
+
+/// CATEGORY ROUTES ///
+
+//GET request for creating a Category. This must come before routes that uses id's.
+router.get('/category/create', category_controller.category_create_get)
+
+router.post('/category/create', category_controller.category_create_post)
+
+router.get('/category/:id/delete', category_controller.category_delete_get)
+
+router.post('/category/:id/delete', category_controller.category_delete_post)
+
+router.get('/category/:id/update', category_controller.category_update_get)
+
+router.post('/category/:id/update', category_controller.category_update_post)
+
+//GET request for a specific Category
+router.get('/category/:id', category_controller.category_detail)
+
+//GET request for list of all Categories
+router.get('/categories', category_controller.category_list)
+
+
+/// PARTINSTANCE ROUTES ///
 
